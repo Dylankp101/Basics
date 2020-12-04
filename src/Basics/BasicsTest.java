4280f12d6a2febfaef4f7cb87aec6b1a4a75fbf3
Dylan KP <60126374+Dylankp101@users.noreply.github.com>
2020-12-04 13:13:45 -0800

    first commit

---
+++ src/Basics/BasicsTest.java
@@ -0,0 +1,23 @@
+package Basics;
+
+//import java.util.ArrayList;
+
+public class BasicsTest {
+	public static void main(String[] args) {
+		Basics iD = new Basics();
+		iD.printAll(256);
+		iD.printOdd(256);
+		iD.sum(255);
+		iD.itterateArray();
+		iD.findMax();
+		iD.average();
+		iD.arrayOdd(256);
+		iD.greaterThan();
+		iD.squareIt();
+		iD.noNegativity();
+		iD.maxMinAvg();
+		iD.shifting();
+			
+	}
+
+}
